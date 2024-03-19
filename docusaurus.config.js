dff83961202cf77db0ab63f8169a72f6c4041a54
Anton Bastin <anton.bastin@chirpwireless.io>
2024-03-19 18:02:21 +0100

    Change url and base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,8 +11,8 @@ const projectName = 'docs.chirptoken.io';
 const config = {
   title: 'Chirp',
   tagline: 'Chirp docs',
-  url: 'https://chirpwireless.github.io',
-  baseUrl: '/docs.chirptoken.io/',
+  url: 'https://docs.chirptoken.io',
+  baseUrl: '/',
   organizationName,
   projectName,
   onBrokenLinks: 'warn',
